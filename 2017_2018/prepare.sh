ee2a97e50f8557be8109472f2af34e07b1ecf698
marc <marc@localhost.localdomain>
2017-11-25 12:53:00 +0100

    x

--- 2017_2018/prepare.sh
+++ 2017_2018/prepare.sh
@@ -1,14 +1,24 @@
 #! /bin/bash
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do 
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 git add TP
 git commit -m 'x'
 git push
 
-DIR=`pwd`
 URL=`git config --get remote.origin.url`
 
-cd ../Archives
-BASE=`pwd`
+ENVOI=${DIR}/Envoi
+/bin/rm -rf ${ENVOI}
+mkdir -p ${ENVOI}
+cd ${ENVOI}
+echo "ENVOI : " ${ENVOI}
 
 git clone ${URL}
 rm -rf AMS_I03/.git
@@ -17,10 +27,10 @@ cd AMS_I03/2017_2018/TP
 d=`date +%Y_%m_%d_%H_%M`
 
 dirs=`ls -d I03_TP?`
-echo ${dirs}
 
 for i in ${dirs}
 do
+    rm -f *.tex
     tar cfz ${BASE}/${i}.tgz ${i}
     cp ${BASE}/${i}.tgz ${BASE}/${i}_${d}.tgz
 done
