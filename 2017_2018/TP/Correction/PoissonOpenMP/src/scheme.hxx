1a4cbc9d01c17f09f1de214266aed5c6d6c630df
Marc Tajchman <marc.tajchman@cea.fr>
2018-02-05 17:00:48 +0100

    x

---
+++ 2017_2018/TP/Correction/PoissonOpenMP/src/scheme.hxx
@@ -0,0 +1,57 @@
+/*
+ * scheme.hxx
+ *
+ *  Created on: 5 janv. 2016
+ *      Author: tajchman
+ */
+
+#ifndef SCHEME_HXX_
+#define SCHEME_HXX_
+
+#include <vector>
+#include "values.hxx"
+#include "parameters.hxx"
+#include "timer.hxx"
+
+class Scheme {
+
+public:
+  Scheme() : codeName("Poisson_OpenMP_FineGrain"), m_timers(3)  {
+   m_timers[0].name("init");
+   m_timers[1].name("solve");
+   m_timers[2].name("other");
+   m_duv = 0.0;
+   m_P = NULL;
+   m_t = 0.0;
+   kStep = 0;
+  }
+  ~Scheme();
+  size_t getDomainSize(int dim) const;
+
+  void initialize(const Parameters *P);
+  double present();
+
+  bool iteration();
+  bool solve(unsigned int nSteps);
+  double variation();
+  void terminate();
+  const Values & getOutput();
+  void setInput(const Values & u);
+  void save(const char * /*fName*/);
+  Timer & timer(int k) { return m_timers[k]; }
+  std::string codeName;
+
+protected:
+  double m_t, m_dt, m_lambda;
+  size_t m_n[3];
+  size_t m_dx[3];
+  size_t m_di[3];
+
+  Values m_u, m_v;
+  double m_duv;
+  const Parameters *m_P;
+  std::vector<Timer> m_timers;
+  int kStep;
+};
+
+#endif /* SCHEME_HXX_ */
---
+++ 2017_2018/TP/Correction/PoissonOpenMP/src/values.hxx
@@ -0,0 +1,41 @@
+#ifndef __VALUES__
+#define __VALUES__
+
+#include "parameters.hxx"
+#include <vector>
+#include <iostream>
+
+class Values {
+
+public:
+
+  Values() : m_p(nullptr) {}
+
+  void init(const Parameters * p,
+         double (*f)(double, double, double) = 0L);
+
+  double & operator() (int i,int j,int k) {
+    return m_u[n2*i + n1*j + k];
+  }
+  double operator() (int i,int j,int k) const {
+    return m_u[n2*i + n1*j + k];
+  }
+
+  void plot(int order) const;
+  void swap(Values & other);
+  int size(int i) const { return m_n[i]; }
+  void print(std::ostream &f) const;
+  
+private:
+  
+  int n1, n2;
+  double * m_u;
+  int m_n[3];
+  const Parameters * m_p;
+
+  virtual void allocate(size_t nn);
+  virtual void deallocate();
+};
+				   
+
+#endif
