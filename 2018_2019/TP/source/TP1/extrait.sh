a17b01cd7f91bc690fccb94bcb0c7fb4a85fd415
marc <marc@localhost.localdomain>
2018-11-13 23:57:41 +0100

    x

---
+++ 2018_2019/TP/source/TP1/extrait.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+DIR=`pwd`
+BASE=`basename ${DIR}`
+
+cd ..
+
+rm -rf temp
+mkdir -p temp/${BASE}
+
+cd temp/${BASE}
+cp -rf ${DIR}/src .
+cp ${DIR}/*.sh .
+rm -f extrait.sh
+cd ..
+tar cvfz ${BASE}.tar.gz ${BASE}
+
