60a4903c7f83ecb8d2737fdfe141eba2dcdcedf4
Marc <marc@localhost.localdomain>
2018-11-21 19:55:48 +0100

    x

--- 2018_2019/TP/source/TP2/src/moyenne_openmp_1/moyenne_openmp_1.cxx
+++ 2018_2019/TP/source/TP2/src/moyenne_openmp_1/moyenne_openmp_1.cxx
@@ -2,32 +2,58 @@
 #include "keyPress.hpp"
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
 #include <iomanip>
 
-void init(std::vector<double> & v)
+double mon_sinus(double x)
 {
-  std::srand(std::time(nullptr));
-  int random_variable = std::rand();
-  
-  int i, n = v.size();
-  for (i=0; i<n; i++)
-    v[i] = 0.45 + 0.1*double(std::rand())/RAND_MAX;
+  double y = x, x2 = x*x;
+   int i, imax = 8, m;
+   double coef = x;
+   for (i=1; i<imax; i++) {
+     m = 2*i*(2*i+1);
+     coef *= -x2/m;
+     y += coef;
+   } 
+   return y;
 }
 
-void stat(const std::vector<double> & v, double & moyenne, double & ecart_type)
+void init(std::vector<double> & exact, std::vector<double> & v)
 {
-  double s1 = 0.0, s2 = 0.0;
+  double x, pi = 3.14159;
   int i, n = v.size();
+  
+#pragma omp parallel for shared(x)
   for (i=0; i<n; i++) {
-    s1 += v[i];
-    s2 += v[i]*v[i];
+    x = i*2*pi/n;
+    v[i] = mon_sinus(x);
+    exact[i] = sin(x);
   }
+}
 
-  moyenne = s1/n;
-  ecart_type = sqrt(s2/n - moyenne*moyenne);
+void save(const char *filename,
+	  std::vector<double> & v1,
+	  std::vector<double> & v2)
+{
+  std::ofstream f(filename);
+  int i, n = v1.size();
+  for (i=0; i<n; i++)
+    f << v1[i] << " " << v2[i] << std::endl;
+}
+
+double erreur(const std::vector<double> & v1, const std::vector<double> & v2)
+{
+  double s = 0.0;
+  int i, n = v1.size();
+
+#pragma omp parallel for reduction(+:s)
+  for (i=0; i<n; i++)
+    s += std::abs(v1[i]-v2[i]);
+
+  return s/n;
 }
 
 int main(int argc, char **argv)
@@ -39,16 +65,19 @@ int main(int argc, char **argv)
   Timer t_init, t_moyenne;
 
   t_init.start();
-  std::vector<double> v(n);
-  init(v);
+  std::vector<double> v(n), w(n);
+  init(v, w);
   t_init.stop();
+
+  if (n < 1000)
+    save("sinus.dat", v, w);
   
   t_moyenne.start();
-  double m, e;
-  stat(v, m, e);
+  double e;
+  e = erreur(v, w);
   t_moyenne.stop();
   
-  std::cout << "moyenne : " << m << " ecart-type : " << e
+  std::cout << "erreur : " << e
             << std::endl << std::endl;
   
   std::cout << "time init    : "
