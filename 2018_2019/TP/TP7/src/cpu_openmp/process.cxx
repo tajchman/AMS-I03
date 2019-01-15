45ef7ad6f2eba9270cefdd8da068f753c242c780
marc <marc@localhost.localdomain>
2019-01-15 07:23:24 +0100

    x

--- 2018_2019/TP/TP7/src/cpu_openmp/process.cxx
+++ 2018_2019/TP/TP7/src/cpu_openmp/process.cxx
@@ -8,10 +8,11 @@ cImage process(cImage &imageIn)
   imageOut.color_type = imageIn.color_type;
   imageOut.bit_depth = imageIn.bit_depth;
 
+#pragma omp parallel for private(i,j,c)
   for (i=1; i<imageIn.width-1; i++) {
     for (j=1; j<imageIn.height-1; j++) {
       for (c=0; c<3; c++)
-	imageOut(i,j,c) = 0.5 + 1.2 * (- 4 * imageIn(i,j,c)
+	imageOut(i,j,c) = 0.5 + (- 4 * imageIn(i,j,c)
 				 + imageIn(i+1,j+1,c)
 				 + imageIn(i-1,j-1,c)
 				 + imageIn(i-1,j+1,c)
