59b179ca6e536d4c5e3acb9fc0836321e4313b01
marc <marc@localhost.localdomain>
2019-02-14 00:52:36 +0100

    x

--- 2018_2019/Examen/Q2/Q2.cxx
+++ 2018_2019/Examen/Q2/Q2.cxx
@@ -4,22 +4,7 @@
 #include <vector>
 #include "timer.hxx"
 
-void maxlocal(std::vector<double> & v,
-	      std::vector<int> & imax, std::size_t &nmax) {
-
-  std::size_t i, n = v.size();
-  std::size_t smax = imax.size();
-  nmax = 0;
-
-  for (i=1; i<n-1; i++) {
-    if ((v[i-1] < v[i]) && (v[i] > v[i+1])) {
-      imax[nmax] = i;
-      nmax += 1;
-      if (nmax >= smax) break;
-    }
-  }
-  
-}
+#include "maxlocal.hxx"
 
 int main() {
 
