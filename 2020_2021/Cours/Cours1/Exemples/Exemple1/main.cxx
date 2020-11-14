8ba9db1aaead5d115fb768b344d2af00e5c8cc4a
Marc Tajchman <marc.tajchman@gmail.com>
2020-11-14 17:53:06 +0100

    x

--- 2020_2021/Cours/Cours1/Exemples/Exemple1/main.cxx
+++ 2020_2021/Cours/Cours1/Exemples/Exemple1/main.cxx
@@ -1,6 +1,6 @@
 #include "init.hxx"
 #include "calcul.hxx"
-#include <iostream>
+#include <fstream>
 #include <vector>
 #include <cstdlib>
 
@@ -12,6 +12,18 @@ int main(int argc, char **argv) {
    init(u);
 
    calcul(v, u);
+
+#if MESURE
+   std::ofstream f("results.gnp");
+   f << "set term pdf\n"
+        "set output 'cycles.pdf'\n"
+        "set xlabel 'Itération'\n"
+        "set ylabel 'Cycles'\n"
+        "set title 'Nombre de cycles pour une iteration'\n"
+        "plot 'results.dat' w lp lc 6 lw 3 pt 6 ps 0.5 notitle\n";
+   f.close();
+   (void) system("gnuplot results.gnp");
+#endif
 
    return 0;
  }
