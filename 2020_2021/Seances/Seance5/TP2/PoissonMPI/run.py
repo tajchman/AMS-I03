16f994fe9970acb03dc7de9f28af449c72859c3e
Marc Tajchman <marc.tajchman@cea.fr>
2021-01-07 16:34:50 +0100

    x

---
+++ 2020_2021/Seances/Seance5/TP2/PoissonMPI/run.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+import os, sys, subprocess, argparse, signal, platform
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--nprocs', type=int, default=1)
+parser.add_argument('rest', nargs=argparse.REMAINDER)
+args = parser.parse_args()
+
+p = platform.system()
+print(p)
+if p == 'Windows':
+    compiler = 'Intel'
+elif p == 'Linux':
+    compiler = 'Gnu'
+
+base = os.path.join(os.getcwd(), 'build', 'Release')
+code = os.path.join(base, 'PoissonMPI')
+
+resultsDir = os.path.join(os.getcwd(), 'results', 'Release')
+if not os.path.exists(resultsDir):
+   os.makedirs(resultsDir)
+
+command = ['mpiexec', '-n', str(args.nprocs), code]
+
+
+def get_pid(name):
+    try:
+        l = map(int, subprocess.check_output(["pidof",name]).split())
+    except:
+        l = []
+    return l
+
+pid_before = get_pid('xterm')
+
+try:
+    print("Taper control-C pour arrêter ... ")
+    subprocess.call(command + args.rest, cwd=resultsDir)
+except KeyboardInterrupt:
+    pid_after = get_pid('xterm')
+    for p in pid_after:
+        if not p in pid_before:
+            os.kill(p, signal.SIGINT)
+
+
+
+
+
---
+++ 2020_2021/Seances/Seance5/TP2/PoissonMPI/src/util/arguments.cxx
@@ -0,0 +1,62 @@
+#include "arguments.hxx"
+#include <cstring>
+#include <iostream>
+
+Arguments::Arguments(int argc, char **argv)
+{
+  for (int iarg = 1; iarg < argc; iarg++) {
+    const char *s = argv[iarg];
+
+    if (strlen(s) > 1) {
+      if (s[0] == '-') {
+        s++;
+        if (s[0] == '-') s++;
+        _options.push_back(s + 1);
+      }
+      else {
+        const char * p = strchr(s, '=');
+        if (p) {
+          std::string key(s, p);
+          std::string value(p+1);
+           _arguments[key] = value;
+        }
+      }
+    }
+  }
+}
+
+bool Arguments::options_contains(const char *s) {
+
+   bool v = false;
+   for (auto & e : _options)
+     if (e == s) {
+       v = true;
+       break;
+     }
+
+   return v;
+}
+
+int Arguments::Get(const char * name, int defaultValue)
+{
+  auto t = _arguments.find(name);
+  if (t == _arguments.end())
+     return defaultValue;
+  return strtol(t->second.c_str(), NULL, 10);
+}
+
+double Arguments::Get(const char *name, double defaultValue)
+{
+  auto t = _arguments.find(name);
+  if (t == _arguments.end())
+     return defaultValue;
+  return strtod(t->second.c_str(), NULL);
+}
+  
+const char * Arguments::Get(const char *name, const char * defaultValue)
+{
+  auto t = _arguments.find(name);
+  if (t == _arguments.end())
+     return defaultValue;
+  return t->second.c_str();
+}
