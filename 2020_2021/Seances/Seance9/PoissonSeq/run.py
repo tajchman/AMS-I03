22ebe938cc360efe8e48a3d0e3061b0141514511
tajchman <marc.tajchman@gmail.com>
2021-02-02 15:47:03 +0100

    x'

---
+++ 2020_2021/Seances/Seance9/PoissonSeq/run.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import os, sys, subprocess, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--type', default='Release', 
+                    choices=['Release', 'Debug'])
+parser.add_argument('rest', nargs=argparse.REMAINDER)
+args = parser.parse_args()
+
+base = os.path.join('.', 
+                    'install',
+                    args.type)
+
+code = os.path.join(base, 'PoissonSeq')
+print(code)
+
+subprocess.call([code] + args.rest)
+
+
